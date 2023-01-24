580f6fda5d0b03c0352a72e5fb269e0b6bb5bf8d
Mansab Mir <mansabmirsde@gmail.com>
2023-01-24 23:29:09 +0530

    Invoices

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,33 +1,26 @@
-import { Box, useTheme } from "@mui/material";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Box, useTheme , Typography} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataContacts } from "../../data/mockData";
+import { mockDataInvoices } from "../../data/mockData";
 import { AdminPanelSettingsOutlined } from "@mui/icons-material";
 import { LockOpenOutlined } from "@mui/icons-material";
 import { SecurityOutlined } from "@mui/icons-material";
 import Header from "../../components/Header";
 
-const Contacts = () => {
+const Invoices = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   const columns = [
     { field: "id", headerName: "ID" },
-    { field: "registrarId", headerName: "Regstrar ID" },
+
     {
       field: "name",
       headerName: "Name",
       flex: 1,
       cellClassName: "name-column--cell",
     },
-    {
-      field: "age",
-      headerName: "Age",
-      flex: 1,
-      type: "number",
-      headerAlign: "left",
-      align: "left",
-    },
+
     {
       field: "phone",
       headerName: "Phone Number",
@@ -39,18 +32,20 @@ const Contacts = () => {
       flex: 1,
     },
     {
-      field: "address",
-      headerName: "Address",
-      flex: 1,
+      field: "cost",
+      headerName: "Cost",
+        flex: 1,
+        renderCell: (params) => {
+            <Typography color={colors.greenAccent[500]}>
+              ${params.row.cost}
+          </Typography>
+      }
     },
     {
-      field: "city",
-      headerName: "City",
+      field: "date",
+      headerName: "Date",
       flex: 1,
-    },
-
-    { field: "zipCode", headerName: "Zip Code" },
-  ];
+    }];
   return (
     <Box m='20px'>
       <Header title='CONTACTS' subtitle='List of contacts'></Header>
@@ -88,13 +83,13 @@ const Contacts = () => {
         }}
       >
         <DataGrid
-          rows={mockDataContacts}
+          rows={mockDataInvoices}
           columns={columns}
-          components={{ Toolbar: GridToolbar }}
+         
         ></DataGrid>
       </Box>
     </Box>
   );
 };
 
-export default Contacts;
+export default Invoices;
